5f514fd5a4da19b8495459d98534dc3bc83432ee
kcubani1 <ubanichukwu@gmail.com>
2023-01-15 13:12:48 -0800

    task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv, exit
+    sum = 0
+    if len(argv) == 1:
+        print(sum)
+        exit(0)
+        arg_len = len(argv
+                i = 1
+                while i < arg_len:
+                sum += int(argv[i])
+                i += 1
+                print(sum)
