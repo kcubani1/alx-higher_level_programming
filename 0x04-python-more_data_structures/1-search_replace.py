9c0c76e722622b27438e4dbf6cd332da9bcb880e
kcubani1 <ubanichukwu@gmail.com>
2023-01-17 08:47:31 -0800

    task1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if not my_list:
+        return my_list
+    new_list = []
+    for element in my_list:
+        if element == search:
+            element = replace
+        new_list.append(element)
+    return new_list
